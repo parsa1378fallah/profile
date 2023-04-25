ed55c2c1c2781bc883734b00c9f0158504eddd6b
parsa <parsaddbb@gmail.com>
2023-04-25 07:12:18 +0330

    feat(template) : add image avatar and vazir fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,8 @@ export default {
   head: {
     title: 'profile',
     htmlAttrs: {
-      lang: 'en'
+      lang : 'fa',
+      dir : 'rtl'
     },
     meta: [
       { charset: 'utf-8' },
@@ -21,6 +22,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/styles/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial/>
+  <div>
+    سلام
+  </div>
 </template>
 
 <script>
