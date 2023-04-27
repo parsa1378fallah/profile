6cbcebca7b855be1482e72e8771d6a746cfed9fe
parsa <parsaddbb@gmail.com>
2023-04-27 12:16:14 +0330

    feat(tempalte) : add contact me section

--- pages/index.vue
+++ pages/index.vue
@@ -49,7 +49,39 @@
           </div>
         </div>
       </div>
-    </div>  
+    </div> 
+    <div  class="container mx-auto sm:flex-col border-b-8 border-dotted pt-5 pb-10">
+      <h3 class="font-bold text-gray-700 text-3xl">ارتباط با من</h3>
+      <div  class="grid grid-cols-1 md:grid-cols-2 gap-10 mt-4">
+        <div class="px-4" >
+          <form action="#">
+            <div class="flex flex-col mb-3">
+              <label for="name" class="text-gray-800">نام :</label>
+              <input  type="text" name="name" id="name" class="mt-2 border-2 border-blue-600 focus:outline-none p-2 rounded-full"
+              placeholder="نام خود را وارد کنید ..." />
+            </div>
+            <div class="flex flex-col ">
+              <label for="email" class="text-gray-800">ایمیل :</label>
+              <input  type="text" name="email" id="email" class="mt-2 border-2 border-blue-600 focus:outline-none p-2 rounded-full"
+              placeholder="ایمیل خود را وارد کنید ..." />
+            </div>
+            <div class="flex flex-col ">
+              <label for="description" class="text-gray-800">توضیحات :</label>
+              <textarea  type="text" name="description" id="description" class="mt-2 border-2 border-blue-600 focus:outline-none py-2 px-4 rounded-2xl"
+              placeholder="توضیحات خود را بنویسید ..." cols="8"></textarea>
+            </div>
+            <button type="submit" class="border-2 border-blue-500 bg-white mt-4 py-2 px-4 rounded-lg hover:bg-blue-500 duration-150">ارسال</button>
+          </form>
+        </div>
+        <div class="px-4">
+          <p class="text-gray-900 font-semibold text-lg mb-4">درباه من</p>
+          <div class="flex flex-col sm:flex-row gap-7">
+            <img class="w-28 lg:w-48 h-28  lg:h-48" src="../assets/images//profile.jpg">
+            <p class="text-justify">در طول سالیان سال تجربه کاری در زمینه فرانت اند و تکنولوژی هایی مانند HTML، CSS، JavaScript و vue.js کسب کرده‌ام. توانایی من در طراحی و توسعه وب سایت‌های پاسخگو و با کیفیت بالا، همراه با توانایی من در مدیریت پروژه‌های چندگانه، از اعتماد بسیاری از مشتریان و همکارانم برخوردار است.</p>
+          </div>
+        </div>
+      </div>
+    </div> 
   </div>
 </template>
 <script setup>
